28ab4cb7cdbfeb25bfeee1095181deb73a8325e1
Arnaud <arnaudroberto38@gmail.com>
2026-04-25 20:51:09 -0300

    teste rota

--- app.py
+++ app.py
@@ -159,3 +159,7 @@ def logout():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
+    @app.route('/teste')
+def teste():
+    return "Funcionando!"
